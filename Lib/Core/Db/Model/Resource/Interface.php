82c0082dc5fe4facd95179c502d499f3193b9c5f
Michal Adamiak <chajr@bluetree.pl>
2014-04-30 21:19:13 +0200

    resource interface [0.7.0]
    This version introduces multiple enhancements:
    
    created and implemented methods from resource interface
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   /Lib/Core/Db/Model/Resource/Multi/Abstract.php
                -- Added --
                    $_mainTable
                    $_tables
                    $_tableRelations
                    save
                    load
                    orderBy
                    page
                    pageSize
                    where
                    delete
    
    Modified:   /Lib/Core/Db/Model/Resource/Interface.php
                -- Added --
                    save
                    load
                    orderBy
                    page
                    pageSize
                    where
                    delete
    
    Modified:   /Lib/Core/Db/etc/config.ini
                -- Modified --
                    changed version from 0.6.2 to 0.7.0

--- Lib/Core/Db/Model/Resource/Interface.php
+++ Lib/Core/Db/Model/Resource/Interface.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * base class to create real data models
- * one model == one table in database
+ * data table model interface
  *
  * @package     Core
  * @subpackage  Db
@@ -9,5 +8,11 @@
  */
 interface Core_Db_Model_Resource_Interface
 {
-    
+    public function save();
+    public function load();
+    public function orderBy($rule);
+    public function page($page);
+    public function pageSize($size);
+    public function where($rule);
+    public function delete();
 }
--- Lib/Core/Db/Model/Resource/Multi/Abstract.php
+++ Lib/Core/Db/Model/Resource/Multi/Abstract.php
@@ -11,5 +11,55 @@ class Core_Db_Model_Resource_Multi_Abstract
     extends Core_Blue_Model_Object
     implements Core_Db_Model_Resource_Interface
 {
+    protected $_mainTable;
+
+    protected $_tables = [];
+
+    protected $_tableRelations;
     
+    public function save()
+    {
+        
+    }
+
+    public function load()
+    {
+        
+    }
+
+    public function orderBy($rule)
+    {
+        
+    }
+
+    public function page($page)
+    {
+        
+    }
+
+    public function pageSize($size)
+    {
+        
+    }
+
+    public function where($rule)
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+    
+    /*
+     * struktura
+     * 
+     * wszystkie tabele do pobrania trzymane w tablicy jako klucz=>kolumny do pobrania
+     * w osobnej tabeli relacje miedzy tabelami
+     * powyzsze stanowia integralna czesc modelu, czyli sa stale dla danej klasy
+     * 
+     * 
+     * filtry ktore odpowiadaja (za to co bedzie pobrane, lub za relacje)
+     */
 }

ef624f612d9e2bd7b0bae2d020c8669a0f2bfa52
Michal Adamiak <chajr@bluetree.pl>
2014-04-30 21:31:06 +0200

    Core update
    This version introduces multiple enhancements:
    
    added math library for Core_Blue, enabled some new modules
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      /Lib/Core/Blue/Helper/Math.php
                -- Added --
                    some mathematic methods
    
    Modified:   /Lib/config.ini
                -- Added --
                    enabled core_time and core_image modules
                -- Updated --
                    changed version from 0.16.0 to 0.17.0
    
    Modified:   /Lib/Core/Blue/etc/config.ini
                -- Modified --
                    changed version from 0.6.2 to 0.7.0

---
+++ Lib/Core/Blue/Helper/Math.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * contains some helpful mathematics models
+ *
+ * @package     Core
+ * @subpackage  Math
+ * @author      chajr   <chajr@bluetree.pl>
+ */
+class Core_Blue_Helper_Math
+{
+    /**
+     * calculate with percent is one number of other
+     *
+     * @param float $part value that is percent of other value
+     * @param float $all value to check percent
+     * @return integer|boolean return FALSE if $all was 0 value
+     */
+    static function numberToPercent($part, $all)
+    {
+        if ($all === 0) {
+            return FALSE;
+        }
+
+        return ($part / $all) *100;
+    }
+
+    /**
+     * calculate percent form value
+     *
+     * @param float $part value that will be percent of other value
+     * @param float $all value from calculate percent
+     * @return integer
+     */
+    static function percent($part, $all)
+    {
+        if ($all === 0) {
+            return FALSE;
+        }
+
+        return ($part / 100) *$all;
+    }
+
+    /**
+     * estimate time to end, by given current usage value and max value
+     *
+     * @param float $edition maximum number of value
+     * @param float $used how many was used
+     * @param integer $start start time in unix timestamp
+     * @return integer estimated end time in unix timestamp
+     */
+    static function end($edition, $used, $start){
+        if (!$used) {
+            return 0;
+        }
+
+        $timeNow = time();
+        $end     = $edition / ($used / ($timeNow - $start));
+
+        $end += $timeNow;
+        return $end;
+    }
+}
